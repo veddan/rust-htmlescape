87dcc520e9a1c480f150aeb1ba74b593dbc0fa50
Viktor Dahl <pazaconyoman@gmail.com>
2013-10-17 10:37:11 +0200

    Explicitly enabled globs and macro_rules

--- src/htmlescape/lib.rs
+++ src/htmlescape/lib.rs
@@ -1,10 +1,16 @@
 #[link(name = "htmlescape",
-       vers = "0.1.0")];
+       vers = "0.1.0",
+       author = "Viktor Dahl",
+       url = "https://github.com/veddan/rust-htmlescape")];
 
 #[comment = "HTML entity-encoding/decoding library"];
 #[crate_type = "lib"];
+
+#[feature(globs)];
+#[feature(macro_rules)];
 
 #[cfg(test)]
 extern mod extra;
 
 pub mod htmlescape;
+
