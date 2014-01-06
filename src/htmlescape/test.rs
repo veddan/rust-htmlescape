22e95c4c232f22a0ffe4a96625e1146db9d997c3
Viktor Dahl <pazaconyoman@gmail.com>
2014-01-06 19:40:51 +0100

    Support rustpkg test

--- src/htmlescape/test.rs
+++ src/htmlescape/test.rs
@@ -1,9 +1,9 @@
 #[crate_id = "htmlescape#0.2.0"];
 #[comment = "HTML entity-encoding/decoding library"];
-#[crate_type = "rlib"];
 
 #[feature(globs)];
 #[feature(macro_rules)];
 
+extern mod extra;
 pub mod htmlescape;
 
