5a914793c6854437ec82d101aa2a3db4f1997888
Viktor Dahl <pazaconyoman@gmail.com>
2014-07-30 02:08:14 +0200

    Start using cargo

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
-#[crate_id = "htmlescape#0.2.0"];
-#[comment = "HTML entity-encoding/decoding library"];
-#[crate_type = "rlib"];
-
-#[feature(globs)];
-#[feature(macro_rules)];
+#![feature(globs)]
+#![feature(macro_rules)]
 
 pub use htmlescape::*;
 
